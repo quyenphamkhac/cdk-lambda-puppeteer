67b42078dbc67a1e5455637f9790b74cd6bce3ab
Quyen Pham Khac <pkquyen@sk-global.biz>
2022-02-25 17:19:27 +0700

    Initial commit

---
+++ test/cdk-lambda-puppeteer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaPuppeteer from '../lib/cdk-lambda-puppeteer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-puppeteer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaPuppeteer.CdkLambdaPuppeteerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
